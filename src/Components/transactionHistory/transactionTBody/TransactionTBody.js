49934d695fdbfe70867db3e98eb60313d15fa6b1
Dmitry Dovgopol <dovgopol.ds@gmail.com>
2021-09-24 18:08:09 +0300

    update task-04 styles

--- src/Components/transactionHistory/transactionTBody/TransactionTBody.js
+++ src/Components/transactionHistory/transactionTBody/TransactionTBody.js
@@ -5,8 +5,12 @@ import TransactionTRow from "./transactionTRow/TransactionTRow";
 const TransactionTBody = ({ transactions }) => {
   return (
     <tbody>
-      {transactions.map((transaction) => (
-        <TransactionTRow key={transaction.id} transaction={transaction} />
+      {transactions.map((transaction, ind) => (
+        <TransactionTRow
+          key={transaction.id}
+          transaction={transaction}
+          ind={ind}
+        />
       ))}
     </tbody>
   );
--- src/Components/transactionHistory/transactionTBody/transactionTRow/TransactionTRow.js
+++ src/Components/transactionHistory/transactionTBody/transactionTRow/TransactionTRow.js
@@ -2,10 +2,10 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./TransactionTRow.module.css";
 
-const TransactionTRow = ({ transaction }) => {
+const TransactionTRow = ({ transaction, ind }) => {
   const { type, amount, currency } = transaction;
   return (
-    <tr>
+    <tr className={ind % 2 && styles.grayBgr}>
       <td className={styles.tableData}>{type}</td>
       <td className={styles.tableData}>{amount}</td>
       <td className={styles.tableData}>{currency}</td>
